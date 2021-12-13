366f6f656146b6a6072407f3f6beb9e3248bf2f1
miriam-castr0 <75701427+miriam-castr0@users.noreply.github.com>
2021-12-13 21:43:33 +0000

    readline 13/12

--- guiao_1/ex4/readline.c
+++ guiao_1/ex4/readline.c
@@ -1,63 +1,70 @@
-#include <sys/types.h>
-#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
-#include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+int readln(int filedes, void *buf, int nbyte)
+{
+    char *buf2 = (char *)buf; //buffer
+    int i = 0;                ///contadores
+    int j = 0;
 
+    while ((j = read(filedes, buf2, nbyte)) != -1) //enquanto houver algo para ler lê
+    {
+        if (*buf2 == '\n') //se clicar enter
+        {
 
-ssize_t readln(int fd, char *line, size_t size);
-
-int main(int argc, const char *argv[]){
-
-    int file1;
-    int bytes;
-    int n=0;
-    
-    char ch[4096];
-
-    file1=open(argv[1], O_RDONLY);
-
-    if(file1 == -1){
-        perror("fail to open");
-        exit(1);
+            return i - 1;
+        }
+        else //se
+        {
+            i++; //incrementa numero de linhas
+        }
     }
 
-    while((n=readln(file1, ch, sizeof(ch)))>0){
-        write(1, ch, n);
+    return -1;
+}
 
+int main(int argc, char **argv)
+{
+    char buf[20];
+    int nbyte = 1;
+    //int i=readln(0,buf,nbyte);
+
+    int fileID;
+    char buff[20];
+    int count;
+    int lineCount = 0;
+    char lineNum[8], lastChar = '\n';
+    int x, y;
+    char *charPtr;
+    for (x = 1; x < argc; x++)
+    {
+        fileID = open(argv[1], O_RDONLY);
+
+        count = 20;
+
+        while (count == 20)
+        {
+            count = read(fileID, buff, 20);
+
+            charPtr = buff;
+            for (y = 0; y < count; y++)
+            {
+                if (lastChar == '\n')
+                {
+                    lineCount++;
+                    sprintf(lineNum, "\t%d  ", lineCount);
+                    write(1, lineNum, strlen(lineNum));
+                }
+
+                lastChar = *charPtr;
+                write(1, charPtr++, 1);
+            }
+        }
+
+        close(fileID);
     }
-        
-    close(file1);
-
 
     return 0;
 }
-
-ssize_t readln(int fd, char *line, size_t size){
-    int i=0, j=0;
-    char ch[size];
-
-    while(read(fd, ch, sizeof(ch))>0){
-        i=0;
-        j=0;
-        
-        for(j=0;j<size; j++){
-            line[i]=ch[j];
-            if(ch[j] == '\n'){
-                
-                return i;
-            }
-            if(j==size-1){
-                return i;
-            }
-            i++;
-        }
-        
-    }
-
-
-
-    
-    return i;
-    }
