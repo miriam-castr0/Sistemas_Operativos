47922de410ed752d23a7e40b3021484ca1b93158
miriam-castr0 <75701427+miriam-castr0@users.noreply.github.com>
2021-12-14 19:47:53 +0000

    read final!!

--- guiao_1/ex4/read.c
+++ guiao_1/ex4/read.c
@@ -4,25 +4,34 @@
 #include <string.h>
 #include <stdlib.h>
 
-int charcount=0;
+#define MAX 1
+
+int charcount = 0;
 
 ssize_t readln(int fd, char *buf, ssize_t nbyte)
 {
     int n, i = 0;
-   
+
+
     while ((n = read(fd, buf, nbyte)) > 0)
-    {  
-        while(buf[i]!='\n'&&i<sizeof(buf))
-        {   
+    {
+        while (buf[i] != '\n' && i < strlen(buf))
+        {
             i++;
             charcount++;
         }
-        i++;
-        charcount++;
-        break;
+       
+        if (buf[i] == '\n' )
+        {
+            i++;
+            charcount++;
+            break;
+        }
+        if(i>=strlen(buf)) break;
+
     }
+
     lseek(fd, charcount, SEEK_SET);
-    
 
     return i;
 }
@@ -31,7 +40,7 @@ int main(int argc, char *argv[])
 {
     int n = 0;
     int fd;
-    char buffer[20];
+    char buffer[MAX];
     if (argc > 1)
     {
         fd = open(argv[1], O_RDONLY);
@@ -40,9 +49,9 @@ int main(int argc, char *argv[])
             perror("Erro a abrir ficheiro");
         }
     }
-    while ((n = readln(fd, buffer, 20)) > 0)
+    while ((n = readln(fd, buffer, MAX)) > 0)
     {
-
+        //printf("%d\n", n);
         write(1, buffer, n);
     }
     close(fd);
